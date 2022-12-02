faa45eab4bbf2d73ac5f6bdd41305f807187664f
MIracleyin <miracleyin@live.com>
2022-12-02 15:18:00 +0800

    hash update

---
+++ src/rust/hashmap/README.md
@@ -0,0 +1,6 @@
+哈希表用来快速判断一个元素是否出现集合里。
+
+1. vec as hash
+2. set as hash
+3. map as hash
+4. hash add two pointer
--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,12 +1,3 @@
 mod array;
 mod listnode;
 mod hashmap;
-
-mod easy_242;
-mod easy_349;
-mod easy_202;
-mod easy_1;
-mod medium_454;
-mod easy_383;
-mod medium_15;
-mod medium_18;
