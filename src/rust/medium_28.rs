f5e6d1248f78c704994aef935bcb4c3a52267671
MIracleyin <miracleyin@live.com>
2022-12-04 19:34:00 +0800

    28 v1

--- src/rust/medium_28.rs
+++ src/rust/medium_28.rs
@@ -6,21 +6,75 @@
 struct Solution;
 // @lc code=start
 impl Solution {
-    pub fn get_next(next: &mut Vec<usize>, s: &Vec<char>) {
+    pub fn get_next_v1(s: &Vec<char>) -> Vec<usize> {
         let len = s.len();
         let mut j = 0;
-        for i in 1..len { // i0 = j + 1
-            while j > 0 && s[i] != s[j] { // 
-                j = next[j - 1];
+        let mut next = vec![0; len];
+        for i in 1..len {
+            // i0 = j + 1
+            while j > 0 && s[i] != s[j] {
+                //
+                j = next[j - 1]; // perfix table
             }
 
             if s[i] == s[j] {
-                j += 1
+                j += 1 
             }
 
             next[i] = j; // j is per post same string len
         }
+        next
     }
+
+    pub fn str_str_v1(haystack: String, needle: String) -> i32 {
+        let (h_len, n_len) = (haystack.len(), needle.len());
+        if h_len == 0 {
+            return 0;
+        }
+        if h_len < n_len {
+            return -1;
+        }
+        let (haystack, needle) = (
+            haystack.chars().collect::<Vec<char>>(),
+            needle.chars().collect::<Vec<char>>(),
+        );
+
+        let next = Self::get_next_v1(&needle);
+
+        let mut j = 0;
+        for i in 0..h_len {
+            while j > 0 && haystack[i] != needle[j] {
+                j = next[j - 1];
+            }
+            if haystack[i] == needle[j] {
+                j += 1;
+            }
+            if j == n_len {
+                return (i - n_len + 1) as i32;
+            }
+        }
+        -1
+    }
+
+    pub fn get_next(s: &Vec<char>) -> Vec<i32>{
+        let len = s.len();
+        let mut next = vec![-1; len];
+        let mut j = -1;
+        for i in 1..len { // here i from 1 because next[i] always is 0
+            // s[i] != s[j]
+            while j > -1 && s[i] != s[(j + 1) as usize] {
+                j = next[j as usize] // iter prefix table
+            }
+            // s[i] s[j]
+            if s[i] == s[(j + 1) as usize] {
+                j += 1;
+            }
+            next[i] = j;
+        }
+
+        next
+    }
+
 
     pub fn str_str(haystack: String, needle: String) -> i32 {
         let (h_len, n_len) = (haystack.len(), needle.len());
@@ -30,23 +84,28 @@ impl Solution {
         if h_len < n_len {
             return -1;
         }
-        let (haystack, needle) = (haystack.chars().collect::<Vec<char>>(), needle.chars().collect::<Vec<char>>());
 
-        let mut next = vec![0; n_len];
-        Self::get_next(&mut next, &needle);
+        let (haystack, needle) = (
+            haystack.chars().collect::<Vec<char>>(),
+            needle.chars().collect::<Vec<char>>(),
+        );
 
-        let mut j = 0;
+
+
+        let mut j = -1;
+        let next = Self::get_next(&needle);
         for i in 0..h_len {
-            while j > 0 && haystack[i] != needle[j] {
-                 j = next[j - 1];
+            while j > -1 && haystack[i] != needle[(j + 1) as usize] {
+                j = next[j as usize]
             }
-            if haystack[i] == needle[j] {
+            if haystack[i] == needle[(j + 1) as usize] {
                 j += 1;
             }
-            if j == n_len {
-                return (i - n_len + 1) as i32;
+            if j == n_len as i32 - 1{ // full match
+                return (i - n_len + 1) as i32
             }
         }
+
         -1
     }
 }
@@ -60,8 +119,9 @@ mod tests {
     fn it_works() {
         let h = "aabaabaaf".to_string();
         let n = "aabaaf".to_string();
-        let res = Solution::str_str(h, n);
+        // let res = Solution::str_str(h, n);
+        let n = n.chars().collect::<Vec<char>>();
+        let res = Solution::get_next(&n); // 010120
         dbg!(res);
     }
 }
-
