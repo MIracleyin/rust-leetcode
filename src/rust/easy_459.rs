906c02665f5bff070b7b4e9ca8c5326b858eff30
MIracleyin <miracleyin@live.com>
2022-12-05 00:17:30 +0800

    459 rust

--- src/rust/easy_459.rs
+++ src/rust/easy_459.rs
@@ -6,16 +6,16 @@
 struct Solution;
 // @lc code=start
 impl Solution {
-    pub fn repeated_substring_pattern(s: String) -> bool {
+    pub fn repeated_substring_pattern_v1(s: String) -> bool {
         let len = s.len();
         if len < 2 {
             return false;
         }
-        for i in 1..=len/2 {
+        for i in 1..=len / 2 {
             if len % i == 0 {
                 let tmp = s.get(0..i).unwrap();
                 let mut res = String::new();
-                for _j in 0..len/i {
+                for _j in 0..len / i {
                     res += tmp;
                 }
                 if res == s {
@@ -24,6 +24,20 @@ impl Solution {
             }
         }
         false
+    }
+
+    pub fn repeated_substring_pattern(s: String) -> bool {
+        (1..s.len())
+            .filter(|&step| s.len() % step == 0) // 1 2
+            .map(|step| { 
+                (step..s.len()) // 1..4 2..4
+                    .step_by(step) //1,2,3,4 2,4
+                    .all(|front| &s[front..step + front] == &s[..step]) 
+                    // &s[1..1+1] == &s[..1] &s[2..1+2] == &s[..2] 
+                    // &s[2..2+2] == &s[..2] 
+                    // Tests if [every] element of the iterator matches a predicate.
+            })
+            .any(|x| x) //Tests if [any] element of the iterator matches a predicate.
     }
 }
 // @lc code=end
@@ -38,4 +52,3 @@ mod tests {
         dbg!(res);
     }
 }
-
