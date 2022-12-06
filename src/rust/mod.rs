ad0f6a63a1a8feecab29c66f3a815b2f2707314e
MIracleyin <miracleyin@live.com>
2022-12-06 17:07:45 +0800

    stack and queue

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -2,11 +2,4 @@ mod array;
 mod listnode;
 mod hashmap;
 mod string;
-
-mod easy_232;
-mod easy_225;
-mod easy_20;
-mod easy_1047;
-mod medium_150;
-mod hard_239;
-mod medium_347;
+mod stack_queue;
---
+++ src/rust/stack_queue/mod.rs
@@ -0,0 +1,7 @@
+mod easy_232;
+mod easy_225;
+mod easy_20;
+mod easy_1047;
+mod medium_150;
+mod hard_239;
+mod medium_347;
