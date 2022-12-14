0b517a38f943ed4ea8aeccc1ad4349fdf11ce68c
MIracleyin <miracleyin@live.com>
2022-12-14 18:18:47 +0800

    backtracking

---
+++ src/rust/backtracking/mod.rs
@@ -0,0 +1,15 @@
+mod medium_77;
+mod medium_216;
+mod medium_17;
+mod medium_39;
+mod medium_40;
+mod medium_131;
+mod medium_93;
+mod medium_78;
+mod medium_90;
+mod medium_491;
+mod medium_46;
+mod medium_47;
+
+mod hard_51;
+mod hard_37;
--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -7,19 +7,3 @@ mod stack_queue;
 mod string;
 mod tree;
 mod backtracking;
-
-mod medium_77;
-mod medium_216;
-mod medium_17;
-mod medium_39;
-mod medium_40;
-mod medium_131;
-mod medium_93;
-mod medium_78;
-mod medium_90;
-mod medium_491;
-mod medium_46;
-mod medium_47;
-
-mod hard_51;
-mod hard_37;
