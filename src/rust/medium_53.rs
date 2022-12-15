17373b8be29a8713c4ed7d2c33e54538f55769f3
MIracleyin <miracleyin@live.com>
2022-12-15 14:13:54 +0800

    53 dp

--- src/rust/medium_53.rs
+++ src/rust/medium_53.rs
@@ -20,7 +20,7 @@ impl Solution {
         result
     }
 
-    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    pub fn max_sub_array_v2(nums: Vec<i32>) -> i32 {
         let mut result = i32::MIN;
         let mut count = 0;
         for i in 0..nums.len() {
@@ -30,6 +30,19 @@ impl Solution {
         }
         result
     }
+
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+        let mut dp = vec![0; nums.len()];
+        dp[0] = nums[0];
+        let mut result = nums[0];
+        for i in 1..nums.len() {
+            dp[i] = nums[i].max(dp[i - 1] + nums[i]);
+            result = dp[i].max(result);
+        }
+        result
+    }
 }
 // @lc code=end
-
