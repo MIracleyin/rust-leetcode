7d901154f27002e2137ca463961cbfbc1c8a7257
MIracleyin <miracleyin@live.com>
2022-12-21 11:32:13 +0800

    bag 70

--- src/rust/easy_70.rs
+++ src/rust/easy_70.rs
@@ -6,7 +6,7 @@
 struct Solution;
 // @lc code=start
 impl Solution {
-    pub fn climb_stairs(n: i32) -> i32 {
+    pub fn climb_stairs_v1(n: i32) -> i32 {
         if n <= 1 {
             return n;
         }
@@ -15,6 +15,19 @@ impl Solution {
         dp[2] = 2;
         for i in 3..=n as usize {
             dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        dp[n as usize]
+    }
+
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut dp = vec![0; n as usize + 1];
+        dp[0] = 1;
+        for j in 0..=n as usize {
+            for i in 1..=2 {
+                if j >= i {
+                    dp[j] += dp[j - i];
+                }
+            }
         }
         dp[n as usize]
     }
