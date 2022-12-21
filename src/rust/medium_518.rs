9cfed4e9a837c74cdee57864e6d7e7871c58a363
MIracleyin <miracleyin@live.com>
2022-12-21 12:03:01 +0800

    518

--- src/rust/medium_518.rs
+++ src/rust/medium_518.rs
@@ -8,7 +8,7 @@ use std::vec;
 struct Solution;
 // @lc code=start
 impl Solution {
-    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+    pub fn change_v1(amount: i32, coins: Vec<i32>) -> i32 {
         // dp[j]：凑成总金额j的货币组合数为dp[j]
         let mut dp = vec![0; amount as usize + 1];
         dp[0] = 1;
@@ -24,8 +24,29 @@ impl Solution {
                 dp[j] += dp[j - coins[i] as usize];
             }
         }
-        dbg!(dp.clone());
         dp[amount as usize]
+    }
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+        // 凑足总额为j所需钱币的最少个数为dp[j]
+        let amount  = amount as usize;
+        let mut dp = vec![i32::MAX; amount + 1];
+        dp[0] = 0;
+        // for j in 1..=amount as usize {
+        //     for i in 0..coins.len() {
+        //         if j >= coins[i] as usize && dp[j - coins[i] as usize] != i32::MAX {
+        //             dp[j] = dp[j].min(dp[j - coins[i] as usize] + 1);
+        //         }
+        //     }
+        // }
+        for i in 0..coins.len() {
+            for j in coins[i] as usize..=amount {
+                if dp[j - coins[i] as usize] != i32::MAX {
+                    dp[j] = dp[j].min(dp[j - coins[i] as usize] + 1);
+                }
+            }
+        }
+        dbg!(dp.clone());
+        if dp[amount] == i32::MAX { -1 } else { dp[amount] }
     }
 }
 // @lc code=end
