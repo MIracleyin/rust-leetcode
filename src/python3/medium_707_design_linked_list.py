bacd3676c148c1a7d2c007e414fb50b7e31da8f9
MIracleyin <miracleyin@live.com>
2023-04-05 23:00:20 +0800

    listnode medium 707

---
+++ src/python3/medium_707_design_linked_list.py
@@ -0,0 +1,81 @@
+#
+# @lc app=leetcode id=707 lang=python3
+#
+# [707] Design Linked List
+#
+
+# @lc code=start
+class Node(object):
+    def __init__(self, x=0):
+        self.next = None
+        self.val = x
+
+class MyLinkedList(object):
+
+    def __init__(self):
+        self.head = Node()
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        cur = self.head.next
+        while(index):
+            cur = cur.next
+            index -= 1
+        return cur.val
+        
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = Node(val)
+        cur = self.head
+        while(cur.next):
+            cur = cur.next
+        cur.next = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0:
+            self.addAtHead(val)
+            return
+        elif index == self.size:
+            self.addAtTail(val)
+            return
+        elif index > self.size:
+            return
+    
+        new_node = Node(val)
+        pre = self.head
+        while(index):
+            pre = pre.next
+            index -= 1
+        new_node.next = pre.next
+        pre.next = new_node
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        pre = self.head
+        while(index):
+            pre = pre.next
+            index -= 1
+        pre.next = pre.next.next
+        self.size -= 1
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+# @lc code=end
+
